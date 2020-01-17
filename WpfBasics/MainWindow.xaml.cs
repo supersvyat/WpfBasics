96ca0dba1265e03638fdedd5a8016505f23724b1
supersvyat <ladyshsyvat@yandex.ru>
2020-01-17 12:41:41 +0400

    Добавьте файлы проекта.

---
+++ WpfBasics/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBasics
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Это описание: {this.DescriptionText.Text}");
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Checkbox1.IsChecked = this.Checkbox2.IsChecked = this.Checkbox3.IsChecked = this.Checkbox4.IsChecked = this.Checkbox5.IsChecked = this.Checkbox6.IsChecked = this.Checkbox7.IsChecked = this.Checkbox8.IsChecked = this.Checkbox9.IsChecked = this.Checkbox10.IsChecked = false;
+        }
+
+        private void Checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.LenghtText.Text += ((CheckBox)sender).Content;
+        }
+
+        private void ComboBoxDropDown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.NoteText == null)
+                return;
+
+            var combo = (ComboBox)sender;
+            var value = (ComboBoxItem)combo.SelectedValue;
+
+            this.NoteText.Text = (string)value.Content;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ComboBoxDropDown_SelectionChanged(this.ComboBoxDropDown, null);
+        }
+
+        private void SupplierNameText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.MassText.Text = this.SupplierNameText.Text; 
+        }
+    }
+}
